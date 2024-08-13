7e1e383af0f6622178a851723a09e8fb7f78bc22
Tahirkhan7 <mahitk98@gmail.com>
2024-08-13 23:51:40 +0530

    added news card

--- src/components/NewsWidget.jsx
+++ src/components/NewsWidget.jsx
@@ -1,15 +1,37 @@
-import { useEffect } from 'react';
-import { fetchNews } from '../api/fetchNews';
+import { useEffect, useState } from "react";
+import { fetchNews } from "../api/fetchNews";
+import styles from "./NewsWidget.module.css";
+import formatDateAndTime from "../utils/formatDateAndTime";
 
 export default function NewsWidget() {
-    useEffect(()=>{
-        fetchNews().then((data)=>{
-            console.log(data);
-        });
-    },[]);
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    fetchNews().then((data) => {
+      if (data.status === "ok") {
+        const index = Math.floor(Math.random() * data.articles.length);
+        setNews(data.articles[index]);
+      }
+    });
+  }, []);
+
+  const renderPublishedAt = (timestamp) => {
+    const { date, time } = formatDateAndTime(timestamp);
+    return `${date} | ${time}`;
+  };
+
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img src={news?.urlToImage} />
+        <div className={styles.heading}>
+          <p>{news?.title}</p>
+          <p>{renderPublishedAt(news?.publishedAt)}</p>
+        </div>
+      </div>
+      <div className={styles.body}>
+        <p>{news?.content}</p>
+      </div>
     </div>
-  )
+  );
 }
