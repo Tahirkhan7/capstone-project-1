31960703fdfe9eb87ca2c57bc5edc48e82d9c253
Tahirkhan7 <mahitk98@gmail.com>
2024-08-15 22:53:35 +0530

    added all notes section

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,5 @@
 import NewsWidget from '../components/NewsWidget';
+import NotesWidget from '../components/NotesWidget';
 import ProfileWidget from '../components/ProfileWidget';
 import TimerWidget from '../components/TimerWidget';
 import WeatherWidget from '../components/WeatherWidget';
@@ -9,9 +10,10 @@ export default function DashboardPage() {
     <div className={styles.container}>
       <div className={styles.profileWidget}><ProfileWidget /></div>
       <div className={styles.weatherWidget}><WeatherWidget /></div>
-      <div className={styles.notesWidget}>notes</div>
+      <div className={styles.notesWidget}><NotesWidget /></div>
       <div className={styles.timerWidget}><TimerWidget /></div>
       <div className={styles.newsWidget}><NewsWidget /></div>
+      <button className={styles.browse}>Browse</button>
     </div>
   );
 }
